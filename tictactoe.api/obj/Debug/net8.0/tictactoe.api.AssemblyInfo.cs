12af4d3e7dc7cff8d3e660ad5536b7a1e7c032a5
Ognjen Vujicic <ognjenvuj@gmail.com>
2025-03-28 21:49:41 +0100

    TicTacToe with PostgreSQL - 3 tier architecture

--- tictactoe.api/obj/Debug/net8.0/tictactoe.api.AssemblyInfo.cs
+++ tictactoe.api/obj/Debug/net8.0/tictactoe.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0f2b91c58b10333843832012c164603c35cc7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
